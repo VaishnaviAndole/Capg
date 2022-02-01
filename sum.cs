53930eab598dc62f73c03490ed88ee29aacdd756
VaishnaviAndole <70020920+VaishnaviAndole@users.noreply.github.com>
2022-02-01 10:27:34 +0530

    Add files via upload

---
+++ sum.cs
@@ -0,0 +1,27 @@
+﻿namespace DemoVaishnavi
+{
+    class Program
+    {
+        static void Main(string[]args){
+ int i,n,sum=0;
+      
+        Console.WriteLine("input");
+                Console.WriteLine("\n");
+
+           n=int.Parse(Console.ReadLine());
+        
+         for(i=1;i<=n;i++)
+         {
+         Console.Write("{0}",i);
+            sum=sum+i;
+         }
+                 Console.WriteLine("\n");
+
+        Console.WriteLine(sum);
+        
+ 
+     }
+    }
+}
+
+
