66a439a5c4f026d26a413efc4f849b51c79e97b3
VaishnaviAndole <70020920+VaishnaviAndole@users.noreply.github.com>
2022-02-01 10:56:37 +0530

    Add files via upload

---
+++ Pattern.cs
@@ -0,0 +1,24 @@
+﻿namespace DemoVaishnavi
+{
+    class Program
+    {
+        static void Main(string[]args){
+ int i,j,r,k=1;
+      
+        Console.WriteLine("input");
+                Console.WriteLine(" \n ");
+
+           r=int.Parse(Console.ReadLine());
+        
+         for(i=1;i<=r;i++)
+         {
+             for(j=1;j<=i;j++)
+         Console.Write(" {0} ",k++);
+         
+                 Console.WriteLine(" \n ");
+         }
+        
+ 
+     }
+    }
+}
